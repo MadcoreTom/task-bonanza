f607b76199e517b396d8c58114d21dc7d1e5f66f
MadcoreTom <madcoretom@gmail.com>
2023-11-13 22:54:30 +1100

    Layout for graph view settings

--- src/components/colummodal.tsx
+++ src/components/colummodal.tsx
@@ -14,10 +14,12 @@ import {
 } from 'gestalt';
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, closeColumnModal } from "../state/store";
-import { COLUMN_TYPES, Column } from "../state/column.types";
+import { COLUMN_TYPES, Column, ColumnType } from "../state/column.types";
+import { MyDropdown } from "./dropdown";
 
 const HEADER_ZINDEX = new FixedZIndex(10);
 const modalZIndex = new CompositeZIndex([HEADER_ZINDEX]);
+const COLUMN_TYPE_OPTIONS = COLUMN_TYPES.map(t => {return{ value: t.name, label: t.name, subtext: t.description }});
 
 export default function ColumnModal() {
     const dispatch = useDispatch();
@@ -30,7 +32,7 @@ export default function ColumnModal() {
 
     const column = headings[columnModalIdx] as Column;
 
-    const [columnMeta, setColumnMeta] = React.useState(column);
+    const [columnMeta, setColumnMeta] = React.useState(column as ColumnType);
 
     return (
         <Layer zIndex={modalZIndex}>
@@ -51,7 +53,7 @@ export default function ColumnModal() {
             >
                 <Box marginBottom={6} minHeight={500}>
                     <Text inline={true}>{column.name} of type {columnMeta.type}</Text>
-                    <ColumnTypeDropdown type={column.type} onClose={t => setColumnMeta({ ...columnMeta, type: t })} />
+                    <MyDropdown value={column.type} options={COLUMN_TYPE_OPTIONS} onClose={t => setColumnMeta({ ...columnMeta, type: t })} />
                 </Box>
             </Modal>
         </Layer>
@@ -62,61 +64,6 @@ export default function ColumnModal() {
 if this has unique values -> option to choose free text or unique 
 if its all numbers or null, then try a number range. you get cool stuff like colour gradients and continuous filters
 */
-
-
-const PAGE_HEADER_ZINDEX = new FixedZIndex(11);
-
-// TODO consider a combobox
-export function ColumnTypeDropdown(props: { type: string, onClose: (type: string) => void }) {
-    const [open, setOpen] = React.useState(false);
-    const x: DropdownOption = {
-        label: props.type,
-        value: props.type
-    }
-    const [selected, setSelected] = React.useState(x);
-    const anchorRef = React.useRef(null);
-
-    const onSelect = ({ item }) => {
-        setSelected(item);
-        setOpen(false);
-        props.onClose(item.value);
-    };
-
-    const options = COLUMN_TYPES.map(t => <Dropdown.Item
-        key={t.name}
-        option={{ value: t.name, label: t.name, subtext: t.description }}
-        onSelect={onSelect}
-        selected={selected}
-    />
-    )
-
-    return (
-        <React.Fragment>
-            <Button
-                accessibilityControls="column-type-dropdown"
-                accessibilityExpanded={open}
-                accessibilityHaspopup
-                iconEnd="arrow-down"
-                onClick={() => setOpen((prevVal) => !prevVal)}
-                ref={anchorRef}
-                selected={open}
-                size="md"
-                text={selected.label}
-            />
-            {open && (
-                <Dropdown
-                    anchor={anchorRef.current}
-                    id="column-type-dropdown"
-                    onDismiss={() => setOpen(false)}
-                    zIndex={new CompositeZIndex([PAGE_HEADER_ZINDEX])}
-                >
-
-                    {options}
-                </Dropdown>
-            )}
-        </React.Fragment>
-    );
-}
 
 /*
 Number
--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { GraphNodes } from "./graph.node";
+import {Button, Flex} from 'gestalt'
+import { MyDropdown } from "./dropdown";
 
 export function Graph(props:{nodes:any}) {
     let [offset, setOffset] = React.useState([0, 0])
@@ -10,7 +12,16 @@ export function Graph(props:{nodes:any}) {
         }
     }
 
-    return <svg height="600"
+    return <React.Fragment>
+        <div style={{height:"100px", marginBottom:"-100px", zIndex:9}}>
+            <Flex alignContent="center" direction="row" alignItems="center" justifyContent="center" gap={1} >
+                 <MyDropdown value={"cat"} options={[{value:"none", label:"None"},{value:"cat",label:"Cat"},{value:"dog",label:"Dog",subtext:"Better than cats"}]} onClose={()=>{}} label={v=>`Colour${v == "none" ? "" : ": "+v}`}/>
+                 <MyDropdown value={"cat"} options={[{value:"none", label:"None"},{value:"cat",label:"Cat"},{value:"dog",label:"Dog",subtext:"Better than cats"}]} onClose={()=>{}} label={v=>`Emoji${v == "none" ? "" : ": "+v}`}/>
+                 <MyDropdown value={"cat"} options={[{value:"none", label:"None"},{value:"cat",label:"Cat"},{value:"dog",label:"Dog",subtext:"Better than cats"}]} onClose={()=>{}} label={v=>`Swimlane${v == "none" ? "" : ": "+v}`}/>
+                 <MyDropdown value={"cat"} options={[{value:"none", label:"None"},{value:"cat",label:"Cat"},{value:"dog",label:"Dog",subtext:"Better than cats"}]} onClose={()=>{}} label={v=>`Row${v == "none" ? "" : ": "+v}`}/>
+            </Flex>
+        </div>
+        <svg height="600"
         id="graph"
         onMouseMoveCapture={onDrag}
         style={{ backgroundPosition: `${offset[0]}px ${offset[1]}px`}}
@@ -21,4 +32,5 @@ export function Graph(props:{nodes:any}) {
         </g>
         <rect x={0} y={0} width={100} height={100} stroke="red" strokeWidth={10} fill="transparent"  />
     </svg>
+    </React.Fragment>
 }
