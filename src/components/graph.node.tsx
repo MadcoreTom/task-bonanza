7d525f82893260a64e31c7fae1a4d850a8796032
MadcoreTom <madcoretom@gmail.com>
2023-11-14 09:41:31 +1100

    Task position is based on column values

--- src/components/graph.node.tsx
+++ src/components/graph.node.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { RootState } from "../state/store";
 import { useSelector } from "react-redux";
 
-function GraphNode(props: { text: string }) {
-    return <g transform={`translate(${Math.random() * 600},${Math.random() * 600})`}>
+function GraphNode(props: { text: string, x: number, y: number }) {
+    return <g transform={`translate(${props.x},${props.y})`} onMouseDown={evt => console.log(props.text)}>
         <rect
             x={0} y={0}
             width={200} height={100}
@@ -20,11 +20,13 @@ export function GraphNodes() {
     const headings = useSelector((state: RootState) => state.main.headings);
     const data = useSelector((state: RootState) => state.main.data);
     const view = useSelector((state: RootState) => state.main.view);
-    const textIdx = headings.map((h,i)=>h.name == view.textColumn ? i : null).filter(x=>x!=null)[0] || 0;
+    const textIdx = headings.map((h, i) => h.name == view.textColumn ? i : null).filter(x => x != null)[0] || 0;
+    const xIdx = headings.map((h, i) => h.type == "X" ? i : null).filter(x => x != null)[0] || 0;
+    const yIdx = headings.map((h, i) => h.type == "Y" ? i : null).filter(x => x != null)[0] || 0;
 
     return <g>
         {
-            data.map(row => <GraphNode text={row[textIdx]} />)
+            data.map(row => <GraphNode text={row[textIdx]} x={parseFloat(row[xIdx])} y={parseFloat(row[yIdx])} />)
         }
     </g>
 }
--- src/state/column.types.ts
+++ src/state/column.types.ts
@@ -2,7 +2,9 @@ export const COLUMN_TYPES: { name: string, description: string }[] = [
     { name: "FREE_TEXT", description: "Any value, with no filtering" },
     { name: "NUMBER", description: "A number, filterable and sortable" },
     { name: "KEYWORD", description: "A set of unique values" },
-    { name: "KEY", description: "The identifier of the row, uniqeunique and never blank"}
+    { name: "KEY", description: "The identifier of the row, uniqeunique and never blank" },
+    { name: "X", description: "x co-ordinate" },
+    { name: "Y", description: "y co-ordinate" }
 ]
 
 export type ColumnType = {
@@ -14,6 +16,8 @@ export type ColumnType = {
 } | {
     type: "KEYWORD",
     // values: string[]
+} | {
+    type: "X" | "Y"
 }
 
 export type Column = {
