416f0291db75a1eb8b3689e9cf397bb2d66fdda4
MadcoreTom <madcoretom@gmail.com>
2024-08-05 23:06:59 +1000

    Improved node sidebar

--- src/components/node.sidebar.tsx
+++ src/components/node.sidebar.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
-import { Switch } from "@nextui-org/react";
+import { Input, Switch } from "@nextui-org/react";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
 import { NodeSelection } from "../model/view.model";
 
 export function NodeSidebar() {
     const selected = useSelector((state: RootState) => state.main.selected as NodeSelection);
+    const record = useSelector((state: RootState) => state.main.records[selected.idx]);
+    const columns = useSelector((state: RootState) => state.main.columns);
 
     return <React.Fragment>
         <h1>Node {selected.idx}</h1>
         <Switch title="MouseDown" isSelected={selected.mouseDown} />
+        {
+            columns.map((c,i)=>{
+                return  <Input
+                isReadOnly
+                label={c.name}
+                variant="bordered"
+                value={record.columns[i]}
+                defaultValue="-"
+              />})
+        }
     </React.Fragment>
 }
